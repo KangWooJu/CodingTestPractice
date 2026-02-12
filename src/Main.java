bfee27b6c19f972e82a156d6e1d7b63ffb13b5ca
KangWooJu <space1102@naver.com>
2026-02-12 14:54:27 +0900

    feat : 백준 9012번 업로드

--- src/Main.java
+++ src/Main.java
@@ -11,90 +11,50 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        /* 백준 - 1406번 : 에디터
+        /* 백준 - 9012번 : 괄호
          */
 
-        // 편집기 문자열 받기
-        String line = br.readLine();
-
-        Stack<Character> left = new Stack<>();
-        Stack<Character> right = new Stack<>();
-
-        for(int i=0;i<line.length();i++){
-            left.push(line.charAt(i));
-        }
-        Editor editor = new Editor(left,right);
-
-        // 명령어 개수 입력받기
         int number = Integer.parseInt(br.readLine());
 
         for(int i=0;i<number;i++){
 
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String command = st.nextToken();
+            Stack<Character> stack = new Stack<>();
+            boolean flag = true;
 
-            if(command.equals("P")){
-                editor.doCommand("P",st.nextToken().charAt(0));
-            } else {
-                editor.doCommand(command,'r');
+
+            String s = br.readLine();
+            for(char c:s.toCharArray()){
+                switch (c) {
+                    case '(' : {
+                        stack.push('(');
+                        break;
+                    }
+
+                    case ')' : {
+                        // 스택이 비워져 있는 경우
+                        if(stack.isEmpty()){
+                            flag = false;
+                            break;
+                        } else {
+                            stack.pop();
+                            break;
+                        }
+                    }
+
+                }
             }
-        }
 
-        // 왼쪽 오른쪽 병합하기
-        while(!left.isEmpty()){
-            right.push(left.pop());
-        }
+            if(!stack.isEmpty()){
+                flag = false;
+            }
 
-        // StringBuilder에 모으기
-        while(!right.isEmpty()){
-            sb.append(right.pop());
+            if(flag){
+                sb.append("YES").append("\n");
+            } else {
+                sb.append("NO").append("\n");
+            }
         }
 
         System.out.print(sb);
-
-
     }
-
-    static class Editor{
-
-        Stack<Character> left;
-        Stack<Character> right;
-
-        public Editor(Stack<Character> left,
-                      Stack<Character> right){
-
-            this.left = left;
-            this.right = right;
-        }
-
-        public void doCommand(String command,
-                              Character alphabet){
-
-            switch (command){
-                case "L":
-                    if(!left.isEmpty()){
-                        right.push(left.pop());
-                    }
-                    break;
-
-                case "D":
-                    if(!right.isEmpty()){
-                        left.push(right.pop());
-                    }
-                    break;
-
-                case "B":
-                    if(!left.isEmpty()){
-                        left.pop();
-                    }
-                    break;
-
-                case "P":
-                    left.push(alphabet);
-                    break;
-            }
-        }
-    }
-
-
 }
