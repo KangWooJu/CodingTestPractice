57af034e6cabbf8f7b7b2811d2f9447383a740bc
KangWooJu <space1102@naver.com>
2026-02-13 23:36:45 +0900

    feat : 이진 탐색 구현 업로드

---
+++ src/BinarySearch.java
@@ -0,0 +1,22 @@
+public class BinarySearch {
+
+    int size;
+    int[] array;
+
+
+    public BinarySearch(int size,
+                        int[] array){
+        this.size = size;
+        this.array = array;
+    }
+
+    public void binarySearch(int[] array){
+
+        // 중간값 설정
+        // 중앙값과 검색값을 비교
+        // 선택 구간에 대해서 중간값 결정
+        // 중간값과 검색값을 비교
+        // ( 중간값 == 검색값 ) 일때 , 검색 종료
+
+    }
+}
