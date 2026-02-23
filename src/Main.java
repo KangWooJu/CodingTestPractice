d249e6355b67f845de03688afb034a6c13dab265
KangWooJu <space1102@naver.com>
2026-02-23 17:41:15 +0900

    feat : 백준 1037번 업로드

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -13,34 +11,31 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        /* 백준 - 1158번 : 요세푸스 문제
+        /* 백준 - 1037번 : 약수
          */
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
+        int result;
+        int number = Integer.parseInt(br.readLine());
         List<Integer> list = new ArrayList<>();
 
-        // 초기 세팅
-        for (int i = 1; i <= N; i++) {
-            list.add(i);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        while(st.hasMoreTokens()){
+            list.add(Integer.parseInt(st.nextToken()));
         }
 
-        int index = 0;
-        sb.append("<");
+        Collections.sort(list);
 
-        while (!list.isEmpty()) {
+        int max = Collections.max(list);
+        int min = Collections.min(list);
 
-            index = (index + K - 1) % list.size();
-            sb.append(list.remove(index));
-
-            if (!list.isEmpty()) {
-                sb.append(", ");
-            }
+        if(max == min){
+            result = max*max;
+        } else {
+            result = min * max;
         }
 
-        sb.append(">");
-        System.out.print(sb);
+        System.out.println(result);
+
     }
 }
